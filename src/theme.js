195c77ef290df253faec3830f79c1b1693281243
Rijan020 <rijandhakal020@gmail.com>
2025-08-10 00:22:05 +0545

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const lightTheme = {
+  background: 'linear-gradient(to top right, #f3f4f6, #e5e7eb, #f9fafb)',
+  text: '#111827'
+};
+
+export const darkTheme = {
+  background: 'linear-gradient(to top right, #111827, #1f2937, #374151)',
+  text: '#f3f4f6'
+};
